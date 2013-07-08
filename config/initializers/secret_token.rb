53f25b400b986baa8f39ef959c649c0559555192
adri <adrianparamo@gmail.com>
2013-07-08 18:37:03 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LarciApp::Application.config.secret_key_base = '805f0fb7daa67ed1ad53b98fdbcff06a5f025885a24bbb3604574d73181952aa74a38e8d5a0ceae857733db8b273f60aa2c220769192a2253edab8ed4e15ceab'
